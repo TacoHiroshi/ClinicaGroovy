b4124da746c518a49066f1d538c072ebe9055411
Hiroshi Taco <hiroshitaco@gmail.com>
2022-07-19 12:58:18 -0500

    Examen Hemato v2

--- vista/examenes/ex_hemograma_completo.php
+++ vista/examenes/ex_hemograma_completo.php
@@ -36,7 +36,7 @@
                             </div>
                             <div class="col-sm-3">
                                 
-                            <p>g%</p>
+                            <p>gr%</p>
                             </div>
                         </div>
                         <div class="row mt-2">
@@ -46,7 +46,7 @@
                             </div>
                             <div class="col-sm-3">
                                 
-                            <p>g%</p>
+                            <p>%</p>
                             </div>
                         </div>
                         <div class="row mt-2">
@@ -96,7 +96,7 @@
                             </div>
                             <div class="col-sm-3">
                                 
-                            <p>g%</p>
+                            <p>fL</p>
                             </div>
                         </div>
                         <div class="row mt-2">
@@ -106,7 +106,7 @@
                             </div>
                             <div class="col-sm-3">
                                 
-                            <p>g%</p>
+                            <p>pg</p>
                             </div>
                         </div>
                         <div class="row mt-2">
@@ -142,13 +142,102 @@
                     </div>
                     
                     <div class="col-4">
-                        <div class="row">
-                            <label for="inputEmail3" class="col-sm-3 col-form-label text-right">Email</label>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Leucocitos</label>
                             <div class="col-sm-4">
                                 <input type="email" class="form-control" id="inputEmail3">
                             </div>
+                            <div class="col-sm-3">
+                                
+                            <p>/mm3</p>
+                            </div>
+                        </div>
+                        
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Plaquetas</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            
+                            <div class="col-sm-3">
+                                
+                            <p>/mm3</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Neutrofilos</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            <div class="col-sm-3">
+                                <p>/%</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Abastonados</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            <div class="col-sm-3">
+                                <p>/%</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Segmentados</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            <div class="col-sm-3">
+                                <p>/%</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Eosinofilos</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            <div class="col-sm-3">
+                                <p>/%</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Basofilos</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            <div class="col-sm-3">
+                                <p>/%</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Linfocitos</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            <div class="col-sm-3">
+                                <p>/%</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <label for="inputEmail3" class="col-sm-4 col-form-label text-right">Monocitos</label>
+                            <div class="col-sm-4">
+                                <input type="email" class="form-control" id="inputEmail3">
+                            </div>
+                            
+                            <div class="col-sm-3">
+                                <p>/%</p>
+                            </div>
                         </div>
                     </div>
+                </div>
+                
+                <div class="row">
+                  <div class="col-md-6 col-sm-3">
+                    <button type="button" class="btn btn-danger" id="cancelar" onclick="c_cancelar()">Cancelar</button>
+                  </div>
+                  <div class="col-md-6 col-sm-3">
+                    <button id="liveAlertBtn" type="submit" class="btn btn-primary text-white text-bold" onclick="revisa()">Continuar <i class="fas fa-arrow-right"></i></button>
+                  </div>
                 </div>
               </div>
             </div>
